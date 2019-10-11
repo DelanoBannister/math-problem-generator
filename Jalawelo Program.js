3429a8a7eba13061b2a796400c3591005e4889db
Icarus-Dragon <54333996+Icarus-Dragon@users.noreply.github.com>
2019-10-10 22:31:25 -0500

    Python to Javascript
    Please give me any tips on how to make it better. The calculations are not a problem for me but to display it and the methods on how it should be displayed i want to know what is you think is most suitable for this program.

---
+++ Jalawelo Program.js
@@ -0,0 +1,18 @@
+/*This Javascript Program will get the information from the user through a HTML form about how many math questions that should be displayed. The Program will also prompt the user for an answer and indicate to the user whether the input was correct or not. The program will go through a loop of continous questions until it reaches the desired amount of inputted*/
+
+var n = document.getElementById("noq").value;//n will be the variable that store the number of questions the user wants
+var i, j;
+for (i = 1; n >= i; i++) {
+    var temp1 = Math.floor(Math.random() * 99) + 1;//A random nuber from 1-99 will be generated and assigned to the variable temp1.
+    var temp2 = Math.floor(Math.random() * 99) + 1;//A random nuber from 1-99 will be generated and assigned to the variable temp1.
+    var t3 = temp2 + temp1;// t3 will be assigned the sum of variables temp1 and temp2.
+    j = document.getElementById("catch").value;//n will be the variable that store the answer the user inputyed for the question
+    if (j == t3) {
+        alert = (" Correct!!!!!!!");// to alert the user that the answer inputted was correct
+    }
+    else {
+        alert = (" Incorrect. The Answer is", t3);// to alert the user that the answer inputted was not correct
+    }
+    document.getElementById("math").innerHTML = " What is " + temp2" + " + temp1;// This line will display all the math questions in html 
+}
+
