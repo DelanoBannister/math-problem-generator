ddf7da33b3c16f66004cfb031a3cb5ca1b306687
DelanoBannister <54333996+DelanoBannister@users.noreply.github.com>
2019-11-05 04:30:01 -0500

    Work-in-progress

---
+++ multiplication.js
@@ -0,0 +1,46 @@
+    let g= require('random');
+    var loop = require('node-while-loop');
+    let temp1= g.int(min = 0, max = 99);
+    let temp2= g.int(min = 0, max = 99);
+    total= temp1 * temp2;
+    console.log('Multiplication:' + temp1 +'multiply by' + temp2);
+    var Array = require('node-array');
+    let ans= [total, total/2, total+total, total + 3];
+    let ans2=['a','b','c','d'];
+    var i = 0;
+    loop.while(function () {
+    return  i = 3;
+}, function () {
+     ans2[i]= ans[g,int(min=i-1, max=i)];
+     i++;
+    });
+    
+    var inq = require('inquirer');
+    inq.prompt([
+    {
+      type: 'checkbox',
+      message: 'Please Select Answer',
+      name: 'Multiplication Answers',
+      choices: [
+        {
+          name: 'A. '+ans2[0]
+        },
+        {
+          name: 'B. '+ans2[1]
+        },
+        {
+          name: 'C. '+ans2[2]
+        },
+        {
+          name: 'D. '+ans2[3]
+        }
+    ],
+    validate: function(answer) {
+      if (answer.length  < 1) {
+        return 'You must choose at ONE answer';
+      }
+
+      return true;
+    }
+  }
+]);
